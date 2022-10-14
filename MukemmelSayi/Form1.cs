8d4627813f821f082f9c6bc82b7d0eb691b3a3ee
erhan.topcu <erhn.topcu@gmail.com>
2022-10-14 14:56:50 +0300

    wf

---
+++ MukemmelSayi/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MukemmelSayi
+{
+    public partial class Form1 : Form
+    {
+        int sayi, toplam;
+        string cevap1 = "Sayı mükemmel bir sayıdır.", cevap2 = "Sayı mükemmel değil.";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayi = Convert.ToInt16(textBox1.Text);
+            toplam = 0;
+            for (int i = 1; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    toplam = toplam + i;
+                }
+            }
+            if (toplam == sayi)
+            {
+                label1.Text = Convert.ToString(cevap1);
+            }
+            else
+            {
+                label1.Text = Convert.ToString(cevap2);
+            }
+        }
+    }
+}
---
+++ TerstenCumleMF/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TerstenCumleMF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string metin, gg;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            metin = Convert.ToString(textBox1.Text);
+            for (int i = metin.Length - 1; i >= 0; i--)
+            {
+                gg = gg + metin[i].ToString();
+            }
+            label1.Text = Convert.ToString(gg);
+        }
+    }
+}
