5dca62fe83abe7a94324b11c01974581ccd67bf5
erhan.topcu <erhn.topcu@gmail.com>
2022-10-21 14:57:11 +0300

    21_10

---
+++ soru_21_10_22/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soru_21_10_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sayi gir");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("CİFTLER");
+            for (int i = 0; i < a; i+=2)
+            {
+                Console.Write(" "+ i*i);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("TEKLER");
+            for (int j = 1; j < a; j+=2)
+            {
+                Console.Write(" " + j*j);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
